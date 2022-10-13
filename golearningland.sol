4d2cb31867160e640591d4bcda9f36c90981fce8
Erdem Ozgen <m.erdemozgen@gmail.com>
2022-10-13 16:07:08 +0300

    Change contract for ipfs uri suffix

--- golearningland.sol
+++ golearningland.sol
@@ -18,20 +18,23 @@ pragma solidity 0.8.7;
 //
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/utils/Strings.sol"; 
 
 contract GoLearningLand is ERC721A, Ownable {
+  using Strings for uint256;
   // "Private" Variables
   address private constant GOPHER1 = 0xf1281D9969b7Cf41063898c8e3BA3eF34589fEaa;
   address private constant GOPHER2 = 0xf1281D9969b7Cf41063898c8e3BA3eF34589fEaa;
   address private constant GOPHER3 = 0xf1281D9969b7Cf41063898c8e3BA3eF34589fEaa;
   address private constant GOPHER4 = 0xf1281D9969b7Cf41063898c8e3BA3eF34589fEaa;
   string private baseURI= "https://address.execute-api.eu-west-1.amazonaws.com/prod/metadata/";
+  string private uriSuffix = ".json"; // can be .json also change test files
 
   // Public Variables
   bool public started = true;
   bool public claimed = false;
   uint256 public constant MAX_SUPPLY = 3333;
-  uint256 public constant MAX_MINT = 1;
+  //uint256 public constant MAX_MINT = 1;
   uint256 public constant TEAM_CLAIM_AMOUNT = 111;
 
   
@@ -73,4 +76,32 @@ contract GoLearningLand is ERC721A, Ownable {
   function enableMint(bool mintStarted) external onlyOwner {
       started = mintStarted;
   }
+
+   function tokenURI(uint256 _tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(_tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        _tokenId.toString(),
+                        uriSuffix
+                    )
+                )
+                : "";
+    }
+        function setUriSuffix(string memory _uriSuffix) public onlyOwner {
+        uriSuffix = _uriSuffix;
+    }
 }
